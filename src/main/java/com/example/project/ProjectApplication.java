fb61352ecbf8fedebbf1b5882dd3874c4d2903e4
YerkhanG <yerkhan12345@gmail.com>
2024-12-13 15:31:08 +0500

    first tut

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -1,11 +1,29 @@
 package com.example.project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import static java.awt.SystemColor.control;
 
 @SpringBootApplication
 public class ProjectApplication {
+    @Profile("usage_message")
+    @Bean
+    public CommandLineRunner usage() {
+        return args -> {
+            System.out.println("This app uses Spring Profiles to control its behavior.\n");
+                    System.out.println("Sample usage: java -jar rabbit-tutorials.jar spring.profiles.active=hello-world,sender");
+        };
+    }
 
+    @Profile("!usage_message")
+    @Bean
+    public CommandLineRunner tutorial() {
+        return new RabbitAmqpTutorialsRunner();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
